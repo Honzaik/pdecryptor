5d690fbe0208f0956389007b4d2224f6e699a693
Jan Oupický <jan.oupicky@internet-handel.cz>
2017-02-25 16:31:12 +0100

    Long string support. Added file input/output.

--- pg.pas
+++ pg.pas
@@ -1,10 +1,8 @@
-//fpc 3.0.0
-
 program Pdecryptor;
-
+{$H+}
 type encryptionKey = array [1..26] of char;
 type letterFrequency = array [1..27] of integer; //27 - number of letters (not counting spaces etc.)
-type candidateArray = array [1..26] of char;
+type candidateArray = array [1..9] of char;
 
 const DEFAULT_BAR = 20;
 
@@ -13,6 +11,7 @@ var rankedLetter : candidateArray = ('e','t','a','o','i','n','s','h','r');
 var letters : letterFrequency;
 var i : byte;
 var ss, encryptedSs : string;
+var readF, writeF : text;
 
 procedure generateKey(var key : encryptionKey);
 var i, newPos : byte;
@@ -24,20 +23,20 @@ begin
         newPos := ((i + Random(26)+1) mod 26) + 1;
         temp := key[newPos];
         key[newPos] := key[i];
-        key[i] := temp;    
+        key[i] := temp;
     end;
 end;
 
 function encryptString(s : string; key : encryptionKey) : string;
-var i, index : byte;
+var i, index : longint;
 var c : char;
 var encryptedS : string;
 begin
-    for i:=1 to length(s) do
+    for i := 1 to length(s) do
     begin
         c := LowerCase(s[i]);
         index := ord(c) - ord('a') + 1;
-        if((index > 0) and (index < 27)) then 
+        if((index > 0) and (index < 27)) then
         begin
             if(c = s[i]) then encryptedS := encryptedS + key[index]
             else encryptedS := encryptedS + UpCase(key[index]);
@@ -48,12 +47,12 @@ begin
 end;
 
 procedure setLetterFrequency(var let : letterFrequency; s : string);
-var i, index : byte;
+var i, index : longint;
 begin
     for i:=1 to length(s) do
     begin
         index := ord(s[i]) - ord('a') + 1;
-        if((index > 0) and (index < 27)) then 
+        if((index > 0) and (index < 27)) then
         begin
             let[index] := let[index] + 1;
             let[27] := let[27] + 1;
@@ -73,9 +72,9 @@ begin
     begin
         if bar <> DEFAULT_BAR then upBar := bar + 0.5
         else upBar := 100;
-        for i:=1 to 26 do 
+        for i:=1 to 26 do
         begin
-            if((100*let[i]/let[27] >= bar) and (100*let[i]/let[27] < upBar)) then 
+            if((100*let[i]/let[27] >= bar) and (100*let[i]/let[27] < upBar)) then
             begin
                 writeln(numberOfCandidates + 1, ' ', chr(i + ord('a') - 1), ' ', 100*let[i]/let[27]:3:0);
                 numberOfCandidates := numberOfCandidates + 1;
@@ -90,17 +89,23 @@ end;
 
 begin
     generateKey(eKey);
-    ss := 'Be at miss or each good play home they. It leave taste mr in it fancy. She son lose does fond bred gave lady get. Sir her company conduct expense bed any. Sister depend change off piqued one. Contented continued any happiness instantly objection.';
     for i:=1 to 26 do write(eKey[i]);
     writeln();
+
+    assign(readF, 'input.txt');
+    assign(writeF, 'output.txt');
+    reset(readF);
+    while not eof(readF) do readln(readF, ss);
+
     writeln(ss);
+    writeln(length(ss));
     encryptedSs := encryptString(ss, eKey);
     writeln(encryptedSs);
     setLetterFrequency(letters, encryptedSs);
     writeln('Numeber of letters: ', letters[27]);
     for i:=1 to 26 do
     begin
-        writeln(chr(i + ord('a') - 1), '->', eKey[i], ' ', letters[ord(eKey[i]) - ord('a') + 1]:3, ' | ', (100*letters[ord(eKey[i]) - ord('a') + 1] / letters[27]):3:0, ' %'); 
+        writeln(chr(i + ord('a') - 1), '->', eKey[i], ' ', letters[ord(eKey[i]) - ord('a') + 1]:3, ' | ', (100*letters[ord(eKey[i]) - ord('a') + 1] / letters[27]):3:0, ' %');
     end;
     getCandidates(letters);
 end.
