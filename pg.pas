8886dd7425a588393a1892c352859068b361b077
Jan Oupický <jan.oupicky@internet-handel.cz>
2017-02-26 23:00:01 +0100

    More frequency analysis.

--- pg.pas
+++ pg.pas
@@ -1,5 +1,10 @@
 program Pdecryptor;
 {$H+}
+type freq = record
+        perc : real;
+        letter : char;
+        eLetter : char
+end;
 type encryptionKey = array [1..26] of char;
 type letterFrequency = array [1..27] of integer; //27 - number of letters (not counting spaces etc.)
 type candidateArray = array [1..9] of char;
@@ -8,7 +13,8 @@ const DEFAULT_BAR = 20;
 
 var eKey : encryptionKey = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 var rankedLetter : candidateArray = ('e','t','a','o','i','n','s','h','r');
-var letters : letterFrequency;
+var frequencies : encryptionKey = ('a', 'i', 'e', 't', 's', 'n', 'o', 'd', 'h', 'r', 'l', 'f', 'm', 'c', 'g', 'y', 'u', 'w', 'p', 'b', 'k', 'v', 'j', 'q', 'x', 'z');
+var letters, bLetters, eLetters, aLetters : letterFrequency;
 var i : byte;
 var ss, encryptedSs : string;
 var readF, writeF : text;
@@ -46,16 +52,35 @@ begin
     encryptString := encryptedS;
 end;
 
-procedure setLetterFrequency(var let : letterFrequency; s : string);
-var i, index : longint;
+procedure setLetterFrequency(var let : letterFrequency; var bLet : letterFrequency; var eLet : letterFrequency; var aLet : letterFrequency; s : string);
+var i, index, l : longint;
+var c : char;
 begin
-    for i:=1 to length(s) do
+    l := length(s);
+    for i:=1 to l do
     begin
-        index := ord(s[i]) - ord('a') + 1;
+        c := LowerCase(s[i]);
+        index := ord(c) - ord('a') + 1;
         if((index > 0) and (index < 27)) then
         begin
             let[index] := let[index] + 1;
             let[27] := let[27] + 1;
+            if((i > 1) and (s[i-1] = ' ')) then
+            begin
+                bLet[index] := bLet[index] + 1;
+                bLet[27] := bLet[27] + 1;
+                if((i+1 <= l) and (s[i+1] = ' ')) then
+                begin
+                    aLet[index] := aLet[index] + 1;
+                    aLet[27] := aLet[27] + 1;
+                end;
+            end
+            else if((i+1 <= l) and (s[i+1] = ' ')) then
+            begin
+                eLet[index] := eLet[index] + 1;
+                eLet[27] := eLet[27] + 1;
+            end;
+
         end;
     end;
 end;
@@ -86,6 +111,42 @@ begin
     getCandidates := output;
 end;
 
+function getSub(c : char; key : encryptionKey) : char;
+var i : byte;
+begin
+        i := 1;
+        while key[i] <> c do i := i+1;
+        getSub := chr(i + ord('a') - 1);
+end;
+
+procedure sumUpFrequencies(var let, bLet, eLet, aLet : letterFrequency; var key : encryptionKey);
+var i, j, min : byte;
+var tempA : array [1..26] of freq;
+var temp : real;
+var tempF : freq;
+begin
+    for i:=1 to 26 do
+    begin
+        temp := (100*let[i] / let[27]);
+        temp := temp + sqrt(100*bLet[i] / bLet[27]);
+        temp := temp + sqrt((100*eLet[i] / eLet[27])*0.25);
+        if(aLet[27] = 0) then aLet[27] := 1;
+        temp := temp + (100*aLet[i] / aLet[27]);
+        tempA[i].perc := temp;
+        tempA[i].letter := chr(ord('a') + i - 1);
+        tempA[i].eLetter := getSub(tempA[i].letter, key);
+        //writeln(chr(ord('a') + i -1), ' ', temp:5:2, ' %');
+    end;
+    for i := 0 to 25 do
+    begin
+       min := i+1;
+       for j:=(1+i) to 26 do if(tempA[j].perc > tempA[min].perc) then min := j;
+       tempF := tempA[i+1];
+       tempA[i+1] := tempA[min];
+       tempA[min] := tempF;
+    end;
+    for i := 1 to 26 do writeln('#', i, ' ', tempA[i].letter, '(', tempA[i].eLetter ,') ', tempA[i].perc:5:2, ' %');
+end;
 
 begin
     generateKey(eKey);
@@ -95,17 +156,23 @@ begin
     assign(readF, 'input.txt');
     assign(writeF, 'output.txt');
     reset(readF);
-    while not eof(readF) do readln(readF, ss);
-
+    while not eof(readF) do
+    begin
+        readln(readF, encryptedSs);
+        ss := ss + encryptedSs;
+    end;
     writeln(ss);
     writeln(length(ss));
     encryptedSs := encryptString(ss, eKey);
     writeln(encryptedSs);
-    setLetterFrequency(letters, encryptedSs);
-    writeln('Numeber of letters: ', letters[27]);
+    setLetterFrequency(letters, bLetters, eLetters, aLetters, encryptedSs);
+    writeln('Number of letters: ', letters[27]);
     for i:=1 to 26 do
     begin
-        writeln(chr(i + ord('a') - 1), '->', eKey[i], ' ', letters[ord(eKey[i]) - ord('a') + 1]:3, ' | ', (100*letters[ord(eKey[i]) - ord('a') + 1] / letters[27]):3:0, ' %');
+        writeln(chr(i + ord('a') - 1), '->', eKey[i], ' ', letters[ord(eKey[i]) - ord('a') + 1]:3, ' | ', (100*letters[ord(eKey[i]) - ord('a') + 1] / letters[27]):5:2, ' % | ',
+        (100*bLetters[ord(eKey[i]) - ord('a') + 1] / bLetters[27]):5:2, ' % | ',
+        (100*eLetters[ord(eKey[i]) - ord('a') + 1] / eLetters[27]):5:2, ' %');
     end;
-    getCandidates(letters);
+    sumUpFrequencies(letters, bLetters, eLetters, aLetters, eKey);
+   // getCandidates(letters);
 end.
